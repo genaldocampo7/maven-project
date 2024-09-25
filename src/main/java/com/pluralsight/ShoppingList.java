a5d998d5832af5fb060a05716065de8456d66034
genald-ocampo <ocampogenald@gmail.com>
2024-09-25 11:18:52 -0700

    completed ShoppingList app

---
+++ src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,16 @@
+package com.pluralsight;
+
+public class ShoppingList {
+     public static void main(String[] args) {
+         System.out.println("apple1");
+         System.out.println("apple2");
+         System.out.println("apple3");
+         System.out.println("apple4");
+         System.out.println("apple5");
+         System.out.println("apple6");
+         System.out.println("apple7");
+         System.out.println("apple8");
+         System.out.println("orange");
+     }
+}
+
